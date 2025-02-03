9961808407a4b87578aa22a7ff5d45ecf639f017
zloygagarko <zloygagarko12@gmail.com>
2025-02-02 18:26:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,6 +91,7 @@ resource "aws_lb_target_group" "example" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.example.id
+  target_type = "ip"
 
   health_check {
     path                = "/health"
